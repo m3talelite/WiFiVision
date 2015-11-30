be43fc0869f5ae708d0ecfab6f1997875c4dc555
Jack <jackxl@hotmail.com>
2015-11-30 14:56:21 +0100

    created the chartplotter class

--- WiFiVision/MainPage.xaml.cs
+++ WiFiVision/MainPage.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,66 @@ namespace WiFiVision
         public MainPage()
         {
             this.InitializeComponent();
+
+            //drawWifiGraph();
+
+            ChartPlotter cp = new ChartPlotter(myCanvas, 400, 500, 400, 400);
+            
+        }
+
+        private void drawWifiGraph()
+        {
+            EllipseGeometry ellipse = new EllipseGeometry();
+            Path circle = new Path();
+            ellipse.Center = new Point(90, 300);
+            ellipse.RadiusX = 30;
+            ellipse.RadiusY = 90;
+            circle.Data = ellipse;
+            circle.Stroke = new SolidColorBrush(Colors.Blue);
+            circle.StrokeThickness = 5;
+            circle.Margin = new Thickness(10);
+
+            myCanvas.Children.Add(circle);
+
+            // Drawing a cruve
+
+            LineGeometry line = new LineGeometry();
+            Path linePath = new Path();
+            line.StartPoint = new Point(200, 200);
+            line.EndPoint = new Point(600, 200);
+            linePath.Data = line;
+            linePath.Stroke = new SolidColorBrush(Colors.Black);
+            linePath.StrokeThickness = 1;
+
+            myCanvas.Children.Add(linePath);
+
+            drawCurve(500,500, 400);
+            drawCurve(540, 500, 400);
+            drawCurve(550, 500, 700);
+        }
+
+        private void drawCurve(double x , double y, double amp)
+        {
+            double curveWidth = 100;
+
+            Path path = new Path();
+            PathFigure figure = new PathFigure();
+            BezierSegment myBs = new BezierSegment();
+
+            myBs.Point1 = new Point(x, y);
+            myBs.Point2 = new Point(x+ curveWidth/2, y- amp);
+            myBs.Point3 = new Point(x+ curveWidth, y);
+
+            figure.Segments.Add(myBs);
+            figure.StartPoint = new Point(x, y); //WUT?!
+
+            PathGeometry myPath = new PathGeometry();
+            myPath.Figures.Add(figure);
+            path.Data = myPath;
+            path.Stroke = new SolidColorBrush(Colors.Red);
+            path.StrokeThickness = 2;
+
+            myCanvas.Children.Add(path);
         }
     }
 }
---
+++ WiFiVision/chartPlotter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace WiFiVision
+{
+    class ChartPlotter
+    {
+        Canvas activeCanvas;
+
+        double x, y, width, height;
+
+        int chartBoxLineThickness = 3;
+        Color chartboxLineColor = Colors.Black;
+
+        public ChartPlotter(Canvas activeCanvas, double x, double y, double width, double height)
+        {
+            this.activeCanvas = activeCanvas;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+
+            drawBox();
+        }
+
+        private void drawBox()
+        {
+            drawLine(new Point(x, y), new Point(x + this.width, y), this.chartBoxLineThickness, this.chartboxLineColor);
+        }
+
+        private void drawLine(Point point1, Point point2, int lineThickness, Color strokeColor)
+        {
+            LineGeometry line = new LineGeometry();
+            Path linePath = new Path();
+            line.StartPoint = point1;
+            line.EndPoint = point2;
+            linePath.Data = line;
+            linePath.Stroke = new SolidColorBrush(strokeColor);
+            linePath.StrokeThickness = lineThickness;
+
+            activeCanvas.Children.Add(linePath);
+        }
+    }
+}
