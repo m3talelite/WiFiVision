f355984887ae1f8c4f36668aac2f7a5efb1b7a0d
m3talelite <Jelle@Braat.NU>
2015-12-07 15:45:05 +0100

    Fixed size of listview items, Custom pivotheader added

---
+++ WiFiVision/PivotHeader.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WiFiVision
+{
+    public sealed partial class PivotHeader : UserControl
+    {
+        public string TabText {
+            get { return _TB.Text; }
+            set { _TB.Text = value; }
+        }
+
+        public Symbol TabSymbol
+        {
+            get { return _SI.Symbol; }
+            set { _SI.Symbol = value; }
+        }
+
+        public PivotHeader()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
